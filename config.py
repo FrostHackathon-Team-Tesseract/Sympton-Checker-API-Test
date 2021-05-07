c0af8e5a33242290a0ff93578cb891c9a93603dc
Nishit Khaitan <nishitk@gmail.com>
2021-05-08 00:58:02 +0530

    initial setup

---
+++ config.py
@@ -0,0 +1,6 @@
+username = "t6DTs_GMAIL_COM_AUT" # Here enter your API user name
+password = "Dy37Arb4EGj26MoTq" # Here enter your API password
+priaid_authservice_url = "https://authservice.priaid.ch" # Be aware that sandbox link is for testing pourpose (not real data) once you get live access you shold use https://authservice.priaid.ch/login
+priaid_healthservice_url = "https://healthservice.priaid.ch" # Be aware that sandbox link is for testing pourpose (not real data) once you get live access you shold use https://healthservice.priaid.ch
+language = "en-gb" # en-gb, de-ch, fr-fr, it-it, es-es, ar-sa, ru-ru, tr-tr, sr-sp, sk-sk...
+pritnRawOutput = False # This flag can be set to see printed json data structure of webservice responses
